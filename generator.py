db7ddb8b7c88a0da7b51c79f5225645cb96cbac0
Preston-Leavitt <pcleavitt@cox.net>
2025-07-27 16:26:30 -0700

    bug fixing

--- generator.py
+++ generator.py
@@ -182,7 +182,7 @@ def interactive_generate(model, tokenizer, device,word):
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 # ===== Flask Web App =====
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/", methods=["GET"])
 def index():

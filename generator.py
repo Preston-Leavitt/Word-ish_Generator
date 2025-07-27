3a41a66878ccadc72dcab1692a1f6451715617ff
Preston-Leavitt <pcleavitt@cox.net>
2025-07-27 15:57:55 -0700

    set PORT

--- generator.py
+++ generator.py
@@ -255,4 +255,5 @@ def guess():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, dubug = False)

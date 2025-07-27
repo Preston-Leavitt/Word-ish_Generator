112bed6de7541c84d090dea55c394607e0348d5b
Preston-Leavitt <pcleavitt@cox.net>
2025-07-27 16:09:27 -0700

    oops, typo

--- generator.py
+++ generator.py
@@ -256,4 +256,4 @@ def guess():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, dubug = False)
+    app.run(host='0.0.0.0', port=port, debug = False)
